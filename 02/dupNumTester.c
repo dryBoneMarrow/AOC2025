5f94df6c4fe3b004f5f259c39153a8cb2b60ccda
Nando Schenk <nando@schenkonline.ch>
2025-12-02 14:27:00 +0100

    clean up

--- 02/dupNumTester.c
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-int main()
-{
-    printf("%lld,%lld", (long long int)(1010 / pow(10, (int)(4 / 2))),
-        (long long int)(1010 % (int)pow(10, (int)(4 / 2))));
-}
